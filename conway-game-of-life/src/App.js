2d4a9734bd60e96c4fb982940e6486560228d7f0
Ariunbold <ariun1118@gmail.com>
2020-09-17 19:36:00 -0400

    created grid

--- conway-game-of-life/src/App.js
+++ conway-game-of-life/src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
+
+const rowNum = 25
+const colNum = 25
 
 function App() {
+  const [grid, setGrid] = useState(() => {
+    const rows = []
+    for (let i = 0; i < rowNum; i++) {
+      rows.push(Array.from(Array(colNum), () => 0))
+    }
+    return rows
+  })
   return (
-    <div className="App">
-      Hello
+    <div className="App" style = {{
+      display: 'grid',
+      gridTemplateColumns: `repeat(${colNum}, 20px)`
+    }}>
+      {grid.map((rows, i) =>
+        rows.map((col, j) => (
+          <div
+            key={`${i} - ${j}`}
+            style={{
+              width: 20,
+              height: 20,
+              background: grid[i][j] ? "green" : undefined,
+              border: 'solid 1px black'
+            }}
+          />
+        ))
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
