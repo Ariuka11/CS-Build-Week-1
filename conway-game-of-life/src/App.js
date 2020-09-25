83970e717447ac4cc2426e657869fa709bf43be6
Ariunbold <ariun1118@gmail.com>
2020-09-24 20:35:54 -0400

    added some ui

--- conway-game-of-life/src/App.js
+++ conway-game-of-life/src/App.js
@@ -80,6 +80,7 @@ function App() {
   speedRef.current = options.speed
 
   const simulation = () => {
+
     setGrid((g) => {
       return produce(g, (gridCopy) => {
         for (let i = 0; i < rowNum; i++) {
@@ -151,6 +152,22 @@ function App() {
   }
   return (
     <div className="container">
+      <div className="rules">
+            <div>
+            <h1>Conway's Game of Life</h1>
+            <p>A  Turing Complete Cellular Automation devised by John Horton Conway</p>
+            </div>
+            <div id="rules">
+                <h4>Rules</h4>
+                <ul>
+                    <li>Any live cell with fewer than two live neighbours dies, as if by underpopulation.</li>
+                    <li>Any live cell with two or three live neighbours lives on to the next generation.</li>
+                    <li>Any live cell with more than three live neighbours dies, as if by overpopulation.</li>
+                    <li>Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</li>
+                    <li>Cells can only live inside the environment, once they try and escape (touch the edge); they die.</li>
+                </ul>
+            </div>
+        </div>
       <div
         className="grid"
         style={{
@@ -188,10 +205,13 @@ function App() {
             onChange={handleChange}
           />
         </form>
-        <div>Generations:{gen}</div>
+        <div className = 'gen'>Generations:{gen}</div>
         <div className="buttons">
           <button
             onClick={() => {
+              if(grid === emptyGrid) {
+                return 
+              }
               setRun(!run)
               if (!run) {
                 runRef.current = true
